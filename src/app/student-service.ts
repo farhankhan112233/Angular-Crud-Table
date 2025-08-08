db67f3c4c1573bfcb2e425f29b8820d6882ca7fb
Farhan Khan <khanjanfarhan2@gmail.com>
2025-08-09 00:39:24 +0500

    CRUD Relational ids | #1

---
+++ src/app/student-service.ts
@@ -0,0 +1,28 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { StudentData } from './Interface/IstudentData';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StudentService {
+  private http = inject(HttpClient);
+  private apiUrl = 'https://localhost:7051/api/Students';
+
+  getAll(): Observable<StudentData[]> {
+    return this.http.get<StudentData[]>(this.apiUrl);
+  }
+
+  create(dto: StudentData): Observable<StudentData> {
+    return this.http.post<StudentData>(this.apiUrl, dto);
+  }
+
+  update(dto: StudentData): Observable<StudentData> {
+    return this.http.put<StudentData>(`${this.apiUrl}/${dto.id}`, dto);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
